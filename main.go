a9c6b4d38e9dd084c58681cfe856b5a63c2099f6
wtlow003 <wtlow003@suss.edu.sg>
2023-08-04 18:08:26 +0800

    boilerplate

--- main.go
+++ main.go
@@ -5,10 +5,5 @@ import "github.com/gin-gonic/gin"
 func main() {
 	gin.SetMode(gin.DebugMode)
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello world!",
-		})
-	})
 	router.Run()
 }
