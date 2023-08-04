e790f45a510d7555188cd48d487c973eb0809233
wtlow003 <wtlow003@suss.edu.sg>
2023-08-04 20:51:03 +0800

    structs for recipe

--- models/recipe.go
+++ models/recipe.go
@@ -11,8 +11,14 @@ type Recipe struct {
 	Name         string             `json:"name" bson:"name"`
 	Tags         []string           `json:"tags" bson:"tags"`
 	Ingredients  []string           `json:"ingredients" bson:"ingredients"`
-	Instructions []string           `json:"instructions" bson:"instruction"`
+	Instructions string             `json:"instructions" bson:"instruction"`
 	Servings     int                `json:"servings" bson:"servings"`
 	Calories     int                `json:"calories" bson:"calories"`
+	Fat          int                `json:"fat" bson:"fat"`
+	SatFat       int                `json:"satfat" bson:"satfat"`
+	Carbs        int                `json:"carbs" bson:"carbs"`
+	Fiber        int                `json:"fiber" bson:"fiber"`
+	Sugar        int                `json:"sugar" bson:"sugar"`
+	Protein      int                `json:"protein" bson:"proten"`
 	PublishedAt  time.Time          `json:"publishedAt" bson:"publishedAt"`
 }
