bc63312ff5db3f429db1ef46d98e869e804e1607
wtlow003 <wtlow003@suss.edu.sg>
2023-08-05 03:35:31 +0800

    modify ID field to be type string

--- models/recipe.go
+++ models/recipe.go
@@ -2,23 +2,21 @@ package models
 
 import (
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Recipe struct {
-	ID           primitive.ObjectID `json:"id" bson:"_id"`
-	Name         string             `json:"name" bson:"name"`
-	Tags         []string           `json:"tags" bson:"tags"`
-	Ingredients  []string           `json:"ingredients" bson:"ingredients"`
-	Instructions string             `json:"instructions" bson:"instruction"`
-	Servings     int                `json:"servings" bson:"servings"`
-	Calories     int                `json:"calories" bson:"calories"`
-	Fat          int                `json:"fat" bson:"fat"`
-	SatFat       int                `json:"satfat" bson:"satfat"`
-	Carbs        int                `json:"carbs" bson:"carbs"`
-	Fiber        int                `json:"fiber" bson:"fiber"`
-	Sugar        int                `json:"sugar" bson:"sugar"`
-	Protein      int                `json:"protein" bson:"proten"`
-	PublishedAt  time.Time          `json:"publishedAt" bson:"publishedAt"`
+	ID           string    `json:"id" bson:"_id"`
+	Name         string    `json:"name" bson:"name"`
+	Tags         []string  `json:"tags" bson:"tags"`
+	Ingredients  []string  `json:"ingredients" bson:"ingredients"`
+	Instructions string    `json:"instructions" bson:"instruction"`
+	Servings     int       `json:"servings" bson:"servings"`
+	Calories     int       `json:"calories" bson:"calories"`
+	Fat          int       `json:"fat" bson:"fat"`
+	SatFat       int       `json:"satfat" bson:"satfat"`
+	Carbs        int       `json:"carbs" bson:"carbs"`
+	Fiber        int       `json:"fiber" bson:"fiber"`
+	Sugar        int       `json:"sugar" bson:"sugar"`
+	Protein      int       `json:"protein" bson:"proten"`
+	PublishedAt  time.Time `json:"publishedAt" bson:"publishedAt"`
 }
