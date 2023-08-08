9f75fda8285b093714eba7cfb4ca86853383e968
wtlow003 <wtlow003@suss.edu.sg>
2023-08-08 22:11:59 +0800

    change id type for mongodb compatibility

--- models/recipe.go
+++ models/recipe.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type UserDefinedRecipe struct {
@@ -20,18 +22,18 @@ type UserDefinedRecipe struct {
 }
 
 type Recipe struct {
-	ID           string    `json:"id" bson:"_id"`
-	Name         string    `json:"name" bson:"name"`
-	Tags         []string  `json:"tags" bson:"tags"`
-	Ingredients  []string  `json:"ingredients" bson:"ingredients"`
-	Instructions string    `json:"instructions" bson:"instruction"`
-	Servings     int       `json:"servings" bson:"servings"`
-	Calories     int       `json:"calories" bson:"calories"`
-	Fat          int       `json:"fat" bson:"fat"`
-	SatFat       int       `json:"satfat" bson:"satfat"`
-	Carbs        int       `json:"carbs" bson:"carbs"`
-	Fiber        int       `json:"fiber" bson:"fiber"`
-	Sugar        int       `json:"sugar" bson:"sugar"`
-	Protein      int       `json:"protein" bson:"proten"`
-	PublishedAt  time.Time `json:"publishedAt" bson:"publishedAt"`
+	ID           primitive.ObjectID `json:"id" bson:"_id"`
+	Name         string             `json:"name" bson:"name"`
+	Tags         []string           `json:"tags" bson:"tags"`
+	Ingredients  []string           `json:"ingredients" bson:"ingredients"`
+	Instructions string             `json:"instructions" bson:"instruction"`
+	Servings     int                `json:"servings" bson:"servings"`
+	Calories     int                `json:"calories" bson:"calories"`
+	Fat          int                `json:"fat" bson:"fat"`
+	SatFat       int                `json:"satfat" bson:"satfat"`
+	Carbs        int                `json:"carbs" bson:"carbs"`
+	Fiber        int                `json:"fiber" bson:"fiber"`
+	Sugar        int                `json:"sugar" bson:"sugar"`
+	Protein      int                `json:"protein" bson:"proten"`
+	PublishedAt  time.Time          `json:"publishedAt" bson:"publishedAt"`
 }
